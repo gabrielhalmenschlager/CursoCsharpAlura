bbb62f792980a884f4acceee74d10c14326e4cc4
“gabrielhalmenschlager” <gabrielh2007.scs@gmail.com>
2025-07-27 14:08:14 -0300

    Preparando o consumo da API

---
+++ ScreenSound.Web/Services/ArtistaApi.cs
@@ -0,0 +1,18 @@
+﻿using ScreenSound.Web.Response;
+using System.Net.Http.Json;
+
+namespace ScreenSound.Web.Services;
+
+public class ArtistaApi
+{
+    private readonly HttpClient _httpClient;
+    public ArtistaApi(IHttpClientFactory factory)
+    {
+        _httpClient = factory.CreateClient("API");
+    }
+
+    public async Task<ICollection<ArtistaResponse>?> GetArtistasAsync()
+    {
+        return await _httpClient.GetFromJsonAsync<ICollection<ArtistaResponse>>("artistas");
+    }
+}
