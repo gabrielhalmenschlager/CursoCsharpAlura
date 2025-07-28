536d629fc5453db1b5f04a72c3d3870b5cecda23
“gabrielhalmenschlager” <gabrielh2007.scs@gmail.com>
2025-07-27 23:04:19 -0300

    Ajustando layout, adicionando logo, aplicando o tema personalizado e corriginado bugs

---
+++ ScreenSound.Web/Services/MusicaApi.cs
@@ -0,0 +1,18 @@
+﻿using ScreenSound.Web.Response;
+using System.Net.Http.Json;
+
+namespace ScreenSound.Web.Services;
+
+public class MusicaApi
+{
+    private readonly HttpClient _httpClient;
+    public MusicaApi(IHttpClientFactory factory)
+    {
+        _httpClient = factory.CreateClient("API");
+    }
+
+    public async Task<ICollection<MusicaResponse>?> GetMusicasAsync()
+    {
+        return await _httpClient.GetFromJsonAsync<ICollection<MusicaResponse>>("musicas");
+    }
+}
