b24d2dba56ead58018e4939bcf8ebeb0950f6397
“gabrielhalmenschlager” <gabrielh2007.scs@gmail.com>
2025-07-28 22:37:19 -0300

    Deletar artista

--- ScreenSound.Web/Services/ArtistaApi.cs
+++ ScreenSound.Web/Services/ArtistaApi.cs
@@ -21,4 +21,14 @@ public class ArtistaApi
     {
         await _httpClient.PostAsJsonAsync("artistas", artista);
     }
+
+    public async Task DeleteArtistaAsync(int id)
+    {
+        await _httpClient.DeleteAsync($"artistas/{id}");
+    }
+
+    public async Task<ArtistaResponse?> GetArtistaPorNomeAsync(string nome)
+    {
+        return await _httpClient.GetFromJsonAsync<ArtistaResponse>($"artistas/{nome}");
+    }
 }
